ac920d3bac4d9a790dd5fff44bd51563ef81937a
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2016-05-12 18:15:45 -0300

    Use GeoModelAdmin when OSMGeoAdmin is not supported

--- brasil_municipios/admin.py
+++ brasil_municipios/admin.py
@@ -1,15 +1,31 @@
 # third-party
 from django.contrib.gis import admin
+from django.core.exceptions import ImproperlyConfigured
 # project
 from .models import Municipio
 
 
-class MunicipioAdmin(admin.OSMGeoAdmin):
-    list_filter = ['state']
-    list_display = ['state', 'id', 'geocode', 'name']
-    list_display_links = ['id', 'geocode', 'name']
-    search_fields = ['name', 'geocode']
-    ordering = ['state', 'name']
-    modifiable = False
+# Create Admin classes dynamically to avoid code repetition
 
-admin.site.register(Municipio, MunicipioAdmin)
+members = {
+    'list_filter': ['state'],
+    'list_display': ['state', 'id', 'geocode', 'name'],
+    'list_display_links': ['id', 'geocode', 'name'],
+    'search_fields': ['name', 'geocode'],
+    'ordering': ['state', 'name'],
+    'modifiable': False,
+}
+
+MunicipioOSMGeoAdmin = type('MunicipioOSMGeoAdmin',
+                            (admin.OSMGeoAdmin,),
+                            members)
+
+MunicipioGeoModelAdmin = type('MunicipioGeoModelAdmin',
+                              (admin.GeoModelAdmin,),
+                              members)
+
+try:
+    admin.site.register(Municipio, MunicipioOSMGeoAdmin)
+except ImproperlyConfigured:
+    # OSMGeoAdmin requirements not met; fallback to standard GeoModelAdmin
+    admin.site.register(Municipio, MunicipioGeoModelAdmin)
